502318734733708f8cd7cd15bfeca090f3b5042c
Kim Taylor <kim.taylor4@live.vu.edu.au>
2014-04-28 11:17:14 +1000

    Verified CRC8 code

--- src/build_tx.c
+++ src/build_tx.c
@@ -7,7 +7,6 @@
 
 #include "build_tx.h"
 
-static int use_ri;
 static int bit_p = 32;
 static uint32_t cur_buf;
 static tx_rate_t tx_rate;
@@ -16,6 +15,30 @@ static void flush(void) {
     fifo_write(cur_buf);
     cur_buf = 0;
     bit_p = 32;
+}
+
+static uint8_t crc8_update(uint8_t crc, uint8_t data) {
+    uint8_t i;
+
+    crc = crc ^ data;
+    for (i = 0; i < 8; i++) {
+        if (crc & 0x01)
+            crc = (crc >> 1) ^ CRC8_POLY;
+        else
+            crc >>= 1;
+    }
+
+    return crc;
+}
+
+static uint8_t crc8(uint32_t header) {
+    uint8_t crc = CRC8_INIT;
+
+    crc8_update(crc, (header & 0xff));
+    crc8_update(crc, ((header >> 8) & 0xff));
+    crc8_update(crc, ((header >> 16) & 0xff));
+    
+    return crc;
 }
 
 static void put_n_bits(uint32_t bits, int num) {
@@ -36,7 +59,7 @@ static void put_n_bits(uint32_t bits, int num) {
     if (bit_p == 0) flush();
 }
 
-static void put_32bit_gold_code(uint32_t code) {
+static void put_bitmap_32(uint32_t code) {
     int i;
     for (i = 0; i < 32; i++) {
 	if (code & (1 << (31 - i))) PUT_SF8_1;
@@ -45,27 +68,61 @@ static void put_32bit_gold_code(uint32_t code) {
 }
 
 void build_tx_preamble(void) {
-    put_32bit_gold_code(PREAMBLE_1);
-    put_32bit_gold_code(PREAMBLE_2);
+    put_bitmap_32(PREAMBLE_1);
+    put_bitmap_32(PREAMBLE_2);
 }
 
-void build_tx_sfd_ri(void) {
-    if (!use_ri) {
-	put_32bit_gold_code(SFD_1);
-	put_32bit_gold_code(SFD_2);
-	return;
-    }
+void build_tx_sfd(void) {
+    put_bitmap_32(SFD_1);
+    put_bitmap_32(SFD_2);
+}
+
+void build_tx_ri(void) {
     switch (tx_rate) {
 	case r_sf_8:
+	    put_bitmap_32(SFD_1);
+	    put_bitmap_32(SFD_2);
+	    put_n_bits(0, 12);
 	    break;
 	case r_sf_4:
+	    put_n_bits(0, 2);
+	    put_bitmap_32(SFD_1);
+	    put_bitmap_32(SFD_2);
+	    put_n_bits(0, 10);
 	    break;
 	case r_sf_2:
+	    put_n_bits(0, 4);
+	    put_bitmap_32(SFD_1);
+	    put_bitmap_32(SFD_2);
+	    put_n_bits(0, 8);
 	    break;
 	case r_sf_1:
+	    put_n_bits(0, 6);
+	    put_bitmap_32(SFD_1);
+	    put_bitmap_32(SFD_2);
+	    put_n_bits(0, 6);
 	    break;
     }
 }
 
-void build_tx_plcp_header(void) {}
+/* Table 82 */
+void build_tx_plcp_header_drf(plcp_header_t *header_info) {
+    uint32_t header_bits;
+
+    header_bits = header_info->data_rate;
+    header_bits |= header_info->pilot_info << 3;
+    header_bits |= header_info->burst_mode << 8;
+    header_bits |= header_info->scrambler_seed << 11;
+    header_bits |= header_info->PDSU_length << 16;
+    header_bits |= crc8(header_bits) << 24;
+}
+
+void build_tx_plcp_header_ri(plcp_header_t *header_info) {
+}
+
 void build_tx_payload(void) {}
+
+void tx_trigger(void) {
+    flush();
+    fifo_trigger();
+}
--- src/build_tx.h
+++ src/build_tx.h
@@ -4,29 +4,58 @@
 #define SFD_1		0x565ddbca
 #define SFD_2		0x58267acd
 
-#define BIT_MAP_8_0	0x55
-#define BIT_MAP_8_1	0xAA
+#define CRC8_POLY	0xB1	/* 10110001 = x^0 + x^2 + x^3 + x^7 + (x^8) */
+#define CRC8_INIT	0xFF
 
-#define BIT_MAP_4_0	0x5
-#define BIT_MAP_4_1	0xA
+#define BIT_MAP_8_0	0x55	/* 01010101 */
+#define BIT_MAP_8_1	0xAA	/* 10101010 */
 
-#define BIT_MAP_2_0	0x2
-#define BIT_MAP_2_1	0x1
+#define BIT_MAP_4_0	0x5	/* 0101 */
+#define BIT_MAP_4_1	0xA	/* 1010 */
 
-#define BIT_MAP_1_0	0x0
-#define BIT_MAP_1_1	0x1
+#define BIT_MAP_2_0	0x1	/* 01 */
+#define BIT_MAP_2_1	0x2	/* 10 */
+
+#define BIT_MAP_1_0	0x0	/* 0 */
+#define BIT_MAP_1_1	0x1	/* 1 */
 
 #define PUT_SF8_0	put_n_bits(BIT_MAP_8_0, 8)
 #define PUT_SF8_1	put_n_bits(BIT_MAP_8_1, 8)
 
+#define PUT_SF4_0	put_n_bits(BIT_MAP_4_0, 4)
+#define PUT_SF4_1	put_n_bits(BIT_MAP_4_1, 4)
+
+#define PUT_SF2_0	put_n_bits(BIT_MAP_2_0, 2)
+#define PUT_SF2_1	put_n_bits(BIT_MAP_2_1, 2)
+
+#define PUT_SF1_0	put_n_bits(BIT_MAP_1_0, 1)
+#define PUT_SF1_1	put_n_bits(BIT_MAP_1_1, 1)
+
 typedef enum {
-    r_sf_8,
-    r_sf_4,
-    r_sf_2,
-    r_sf_1
+    r_sf_8 = 0,
+    r_sf_4 = 1,
+    r_sf_2 = 2,
+    r_sf_1 = 3
 } tx_rate_t;
 
+typedef enum {
+    pilot_64 = 2,
+    pilot_128 = 3
+} pilot_period_t;
+
+typedef struct {
+    tx_rate_t	    data_rate;
+    pilot_period_t  pilot_info;
+    int		    burst_mode;
+    int		    scrambler_seed; /* Fixme - check data type */
+    uint8_t	    PDSU_length;
+    uint8_t	    crc8;
+} plcp_header_t;
+
 extern void build_tx_preamble(void);
-extern void build_tx_sfd_ri(void);
-extern void build_tx_plcp_header(void);
+extern void build_tx_sfd(void);
+extern void build_tx_ri(void);
+extern void build_tx_plcp_header_drf(plcp_header_t *header_info);
+extern void build_tx_plcp_header_ri(plcp_header_t *header_info);
 extern void build_tx_payload(void);
+extern void tx_trigger(void);
